f67fcf5e56255b4cf2a1a453d72b8c126ea74d0e
ArunArunAk <145891383+ArunArunAk@users.noreply.github.com>
2024-02-22 13:38:39 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-    production: true,
-    firebaseConfig : {
-        apiKey: 
-        authDomain: "roadmaster-blog.firebaseapp.com",
-        projectId: "roadmaster-blog",
-        storageBucket: "roadmaster-blog.appspot.com",
-        messagingSenderId: 
-        appId: 
-      }
+   
   };
   
